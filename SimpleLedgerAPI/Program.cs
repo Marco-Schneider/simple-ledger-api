0b731377ed3fb497d871f68a4f76958f1c4d19e3
Marco-Schneider <marco.schneider452@gmail.com>
2026-06-11 21:58:59 -0300

    chore: register in-memory account store as a singleton

--- SimpleLedgerAPI/Program.cs
+++ SimpleLedgerAPI/Program.cs
@@ -1,3 +1,5 @@
+using SimpleLedgerAPI.Store;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IAccountStore, InMemoryAccountStore>();
 
 var app = builder.Build();
 
