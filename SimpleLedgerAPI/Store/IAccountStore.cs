bf20d368d19c864f4a9613e55bec3f0c9427b02d
Marco-Schneider <marco.schneider452@gmail.com>
2026-06-11 21:58:39 -0300

    feat: add in-memory account store

---
+++ SimpleLedgerAPI/Store/IAccountStore.cs
@@ -0,0 +1,11 @@
+﻿using SimpleLedgerAPI.Domain;
+
+namespace SimpleLedgerAPI.Store
+{
+    public interface IAccountStore
+    {
+        Account? GetAccount(string accountId);
+        void SaveAccount(Account account);
+        void Reset();
+    }
+}
---
+++ SimpleLedgerAPI/Store/InMemoryAccountStore.cs
@@ -0,0 +1,26 @@
+﻿using SimpleLedgerAPI.Domain;
+using System.Collections.Concurrent;
+
+namespace SimpleLedgerAPI.Store
+{
+    public class InMemoryAccountStore : IAccountStore
+    {
+        private readonly ConcurrentDictionary<string, Account> _accounts = new();
+
+        public Account? GetAccount(string accountId)
+        {
+            _accounts.TryGetValue(accountId, out var account);
+            return account;
+        }
+
+        public void SaveAccount(Account account)
+        {
+            _accounts.AddOrUpdate(account.Id, account, (key, existingAccount) => account);
+        }
+
+        public void Reset()
+        {
+            _accounts.Clear();
+        }
+    }
+}
