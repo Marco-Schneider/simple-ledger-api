5f31cce1d87192abc837f56ab7f624f3a44c0d23
Marco-Schneider <marco.schneider452@gmail.com>
2026-06-13 00:45:10 -0300

    feat: add /reset and /balance endpoints

---
+++ SimpleLedgerAPI/Controllers/LedgerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleLedgerAPI.Services;
+
+namespace SimpleLedgerAPI.Controllers
+{
+    public class LedgerController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public LedgerController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("/reset")]
+        public IActionResult Reset()
+        {
+            _accountService.Reset();
+            return Ok();
+        }
+
+        [HttpGet("/balance")]
+        public IActionResult GetBalnce([FromQuery(Name = "account_id")] string accountId)
+        {
+            var result = _accountService.GetBalance(accountId);
+
+            if (!result.IsSuccess)
+                return NotFound(0);
+
+            return Ok(result);
+        }
+    }
+}
