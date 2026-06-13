96283be9904b5d4c42c3a06c517acc8b990ebc6b
Marco-Schneider <marco.schneider452@gmail.com>
2026-06-13 01:37:54 -0300

    test: add initial integration tests

---
+++ SimpleLedgerAPI.Test/Integration/LedgerControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using SimpleLedgerAPI.Domain;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace SimpleLedgerAPI.Tests.Integration
+{
+    public class LedgerControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public LedgerControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact(DisplayName = "/event | When and invalid event is requested, should return bad request")]
+        public async Task ProcessEventShouldReturnBadRequestForInvalidEvent()
+        {
+            // Arrange
+            var invalidRequest = new EventRequest("refund", "100", "200", 15m);
+
+            // Act
+            var response = await _httpClient.PostAsJsonAsync("event", invalidRequest);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+    }
+}
--- SimpleLedgerAPI/Program.cs
+++ SimpleLedgerAPI/Program.cs
@@ -1,31 +1,35 @@
 using SimpleLedgerAPI.Services;
 using SimpleLedgerAPI.Store;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddSingleton<IAccountStore, InMemoryAccountStore>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddScoped<IAccountService, AccountService>();
+        builder.Services.AddSingleton<IAccountStore, InMemoryAccountStore>();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
